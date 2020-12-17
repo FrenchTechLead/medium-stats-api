8d612a82dcb0b184b20f7bb295971d9e4310a1a3
akram mecheri <mecheri.akram@gmail.com>
2020-12-17 18:41:54 +0100

    adding domain check

--- Main.py
+++ Main.py
@@ -1,8 +1,8 @@
 import os
 
 import requests
-from flask import Flask, jsonify, request
-from flask_restful import Api, Resource
+from flask import Flask, jsonify, make_response, request
+from flask_restful import Api
 
 app = Flask(__name__)
 app.config['JSONIFY_PRETTYPRINT_REGULAR'] = False
@@ -16,14 +16,17 @@ def hello():
 @app.route('/api/stats', methods=['GET'])
 def getJwt():
     story_url = request.args.get('story_url')
-    c = requests.get(story_url).content.decode("utf-8")
-    c = c.split("clapCount\":")[1]
-    endIndex = c.index(",")
-    claps = int(c[0:endIndex])
-    c = c.split("responsesCount\":")[1]
-    endIndex = c.index(",")
-    comments = int(c[0:endIndex])
-    return jsonify(comments=comments, claps=claps)
+    if not "medium.com" in story_url:
+        return make_response(jsonify(msg="Not authorized"), 403)
+    else:
+        c = requests.get(story_url).content.decode("utf-8")
+        c = c.split("clapCount\":")[1]
+        endIndex = c.index(",")
+        claps = int(c[0:endIndex])
+        c = c.split("responsesCount\":")[1]
+        endIndex = c.index(",")
+        comments = int(c[0:endIndex])
+        return jsonify(comments=comments, claps=claps)
 
 
 api = Api(app)
